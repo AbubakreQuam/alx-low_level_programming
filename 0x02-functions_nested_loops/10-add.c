7463f9a2e1b65752837a58b2ae114872b9bc7bbd
Abubakre Quamarudeen <quamarudeenabubakre@gmail.com>
2023-03-01 17:57:02 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds tow integers
+ * @a: number 1 to be added
+ * @b: number 2 to be added
+ * Return: The result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
