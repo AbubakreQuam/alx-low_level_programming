ec6ab68d82e2b625801a972e5786a51f931cba2c
Abubakre Quamarudeen <quamarudeenabubakre@gmail.com>
2023-03-01 17:44:33 +0100

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks wheter a charecter is lower case or not
+ * @c: character to be checked
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
