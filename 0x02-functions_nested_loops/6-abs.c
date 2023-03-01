97485915601d5fffe6befbb2937673ed9952d276
Abubakre Quamarudeen <quamarudeenabubakre@gmail.com>
2023-03-01 17:50:02 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - gives the absolute value of a number
+ * @n: The number to be in the absolute value
+ * Return: the absolute valued number
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
