c772cf6fd0a20957d4c1551b7753481d6e5debf1
Abubakre Quamarudeen <quamarudeenabubakre@gmail.com>
2023-03-01 17:38:18 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * main - prints Holberton text
- * Return: Always 0 (Success)
- */
+
 
 int main(void)
 {
-	printf('_putchar');
+	printf("_putchar");
 	printf('\n');
 	return (0);
 }
