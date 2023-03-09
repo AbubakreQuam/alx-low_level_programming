94700c4d2d84c3fb64de1477de4a4f4718875f25
Abubakre Quamarudeen <quamarudeenabubakre@gmail.com>
2023-03-09 10:39:10 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,13 @@ void iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
 
-
+/**
+ * strlen_no_wilds - Returns the length of a string,
+ *                   ignoring wildcard characters.
+ * @str: The string to be measured.
+ *
+ * Return: The length.
+ */
 int strlen_no_wilds(char *str)
 {
 	int len = 0, index = 0;
@@ -22,7 +28,11 @@ int strlen_no_wilds(char *str)
 	return (len);
 }
 
-
+/**
+ * iterate_wild - Iterates through a string located at a wildcard
+ *                until it points to a non-wildcard character.
+ * @wildstr: The string to be iterated through.
+ */
 void iterate_wild(char **wildstr)
 {
 	if (**wildstr == '*')
